8fc8287db0a6d67af2c71ff594889d1957a08486
Erdi TASKIN <55998212+erditaskin@users.noreply.github.com>
2025-09-08 00:12:08 +0300

    File UI Components: React component: Component

---
+++ frontend/src/components/Component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ComponentProps {
+  // Add props here
+}
+
+const Component: React.FC<ComponentProps> = () => {
+  return (
+    <div className="component">
+      <h1>Component</h1>
+      {/* Component implementation */}
+    </div>
+  );
+};
+
+export default Component;
